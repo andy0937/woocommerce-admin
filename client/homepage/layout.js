687596f7cef6008098cbd200e9af667368c2b1d7
Matt Sherman <matt@jam123.com>
2020-06-05 12:10:28 -0400

    Only show quick links if task list is not shown.

--- client/homepage/layout.js
+++ client/homepage/layout.js
@@ -52,7 +52,12 @@ export const Layout = ( props ) => {
 		};
 	}, [] );
 
-	const { query, requestingTaskList, taskListComplete, taskListHidden } = props;
+	const {
+		query,
+		requestingTaskList,
+		taskListComplete,
+		taskListHidden,
+	} = props;
 	const isTaskListEnabled = taskListHidden === false && ! taskListComplete;
 	const isDashboardShown = ! isTaskListEnabled || ! query.task;
 
@@ -88,7 +93,7 @@ export const Layout = ( props ) => {
 				>
 					{ isTaskListEnabled && renderTaskList() }
 					<StatsOverview />
-					<QuickLinks />
+					{ ! isTaskListEnabled && <QuickLinks /> }
 				</div>
 			</Fragment>
 		);
@@ -114,8 +119,7 @@ export const Layout = ( props ) => {
 		>
 			{ isDashboardShown
 				? renderColumns()
-				: isTaskListEnabled && renderTaskList()
-			}
+				: isTaskListEnabled && renderTaskList() }
 		</div>
 	);
 };
@@ -141,24 +145,25 @@ Layout.propTypes = {
 
 export default compose(
 	withSelect( ( select ) => {
-		const {
-			getOptions,
-			isGetOptionsRequesting,
-		} = select( 'wc-api' );
+		const { getOptions, isGetOptionsRequesting } = select( 'wc-api' );
 
 		if ( isOnboardingEnabled() ) {
 			const options = getOptions( [
 				'woocommerce_task_list_complete',
 				'woocommerce_task_list_hidden',
 			] );
-			
+
 			return {
 				requestingTaskList: isGetOptionsRequesting( [
 					'woocommerce_task_list_complete',
 					'woocommerce_task_list_hidden',
 				] ),
-				taskListComplete: get( options, [ 'woocommerce_task_list_complete' ] ),
-				taskListHidden: get( options, [ 'woocommerce_task_list_hidden' ] ) === 'yes',
+				taskListComplete: get( options, [
+					'woocommerce_task_list_complete',
+				] ),
+				taskListHidden:
+					get( options, [ 'woocommerce_task_list_hidden' ] ) ===
+					'yes',
 			};
 		}
 
